532f7223bee4eb26a32754914aaca5c26c876385
Avtarso <t0676352927@gmail.com>
2024-05-31 01:03:42 +0300

    push to main

--- main_scrapy.py
+++ main_scrapy.py
@@ -4,8 +4,10 @@ from scrapy.utils.project import get_project_settings
 from quotes_scraper.quotes_scraper.spiders.quotes import QuotesSpider
 from quotes_scraper.quotes_scraper.spiders.authors import AuthorsSpider
 
-process = CrawlerProcess(get_project_settings())
 
-process.crawl(QuotesSpider)
-process.crawl(AuthorsSpider)
-process.start()
+if __name__ == '__main__':
+    process = CrawlerProcess(get_project_settings())
+
+    process.crawl(QuotesSpider)
+    process.crawl(AuthorsSpider)
+    process.start()
